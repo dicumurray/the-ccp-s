5ed27698289ac6b9756acc3b360cfd6d238e5d28
Andres Di Cunzolo <andru@MacBook-Air-de-Andres.local>
2024-11-09 02:27:57 -0300

    Ajustes para hacer el logo responsivo y cambios en el tamaño de fuente

--- script.js
+++ script.js
@@ -1,21 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
     const text = document.getElementById('logoText');
     const textContent = text.textContent;
-    text.innerHTML = ''; // Limpiar el contenido del texto original
+    text.innerHTML = ''; // Limpiar el contenido original del texto
 
     // Crear un efecto de onda en cada letra
     textContent.split('').forEach((char, index) => {
         const span = document.createElement('span');
         span.textContent = char;
-
-        // Si el carácter es un espacio, manejarlo adecuadamente
-        if (char === ' ') {
-            span.style.width = '10px'; // Ajustar el ancho del espacio según sea necesario
-        }
-
-        span.style.display = 'inline-block';
-        span.style.animation = `wave 2s ease-in-out infinite`;
-        span.style.animationDelay = `${index * 0.1}s`;
+        span.style.animationDelay = `${index * 0.1}s`; // Animación secuencial
         text.appendChild(span);
     });
 });
